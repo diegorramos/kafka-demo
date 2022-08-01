d991af49ed9b5627b0a9bd50030a36033cb43745
Diego Rocha <diego.ramos@creditas.com>
2022-08-01 12:25:56 -0300

    Add consumer and producer on project

--- src/main/kotlin/io/diegorxramos/kafkademo/infrastructure/KafkaConsumerConfig.kt
+++ src/main/kotlin/io/diegorxramos/kafkademo/infrastructure/KafkaConsumerConfig.kt
@@ -3,7 +3,6 @@ package io.diegorxramos.kafkademo.infrastructure
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
