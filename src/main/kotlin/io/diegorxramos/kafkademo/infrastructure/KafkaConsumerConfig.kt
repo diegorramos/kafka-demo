0a39179e2b6d433636cd5ddd2e7645a0f16637cb
Diego Rocha <diego.ramos@creditas.com>
2022-08-01 12:19:10 -0300

    Add consumer and producer on project

--- src/main/kotlin/io/diegorxramos/kafkademo/infrastructure/KafkaConsumerConfig.kt
+++ src/main/kotlin/io/diegorxramos/kafkademo/infrastructure/KafkaConsumerConfig.kt
@@ -2,6 +2,7 @@ package io.diegorxramos.kafkademo.infrastructure
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,14 +14,14 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 
 @Configuration
 class KafkaConsumerConfig(
-    private val properties: KafkaProperties
+    @Value("\${spring.kafka.bootstrap-servers}") val bootstrapAddress: String
 
     ) {
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, Any> {
         val config = mapOf<String, Any>(
-            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.bootstrapServers,
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapAddress,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java
         )
